2a572282c704c3682875c16deac577c02d9c8c7f
Donice <doniceubaru1@gmail.com>
2022-11-11 10:05:56 +0100

    finished home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,41 @@
 import React from "react";
 import heroImage from "./../img/hero.svg";
-import banner1 from './../img/banner1.svg'
-import banner2 from './../img/banner2.svg'
-import banner3 from './../img/banner1.svg'
+import heroImage2 from "./../img/hero2.svg";
+import banner1 from "./../img/banner1.svg";
+import banner2 from "./../img/banner2.svg";
+import banner3 from "./../img/banner1.svg";
 
-import One from './../img/cards/1.svg'
-import Two from './../img/cards/2.svg'
-import Three from './../img/cards/3.svg'
-import Four from './../img/cards/4.svg'
-import Five from './../img/cards/5.svg'
-import Six from './../img/cards/6.svg'
-import Seven from './../img/cards/7.svg'
-import Eight from './../img/cards/8.svg'
-import Nine from './../img/cards/9.svg'
-import Ten from './../img/cards/10.svg'
-import Eleven from './../img/cards/11.svg'
-import Twelve from './../img/cards/12.svg'
+import One from "./../img/cards/1.svg";
+import Two from "./../img/cards/2.svg";
+import Three from "./../img/cards/3.svg";
+import Four from "./../img/cards/4.svg";
+import Five from "./../img/cards/5.svg";
+import Six from "./../img/cards/6.svg";
+import Seven from "./../img/cards/7.svg";
+import Eight from "./../img/cards/8.svg";
+import Nine from "./../img/cards/9.svg";
+import Ten from "./../img/cards/10.svg";
+import Eleven from "./../img/cards/11.svg";
+import Twelve from "./../img/cards/12.svg";
+
+// import BsStarFill from 'react-icons/bs'
 
 const Home = () => {
-  const places = [One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Eleven, Twelve]
-  
+  const places = [
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Eleven,
+    Twelve,
+  ];
+
   return (
     <div>
       <div className="m-8 md:justify-between md:flex">
@@ -53,28 +69,58 @@ const Home = () => {
       </div>
 
       <div className="flex bg-fuchsia-900 my-4 w-full h-12">
-        <img src={banner1} alt="banner1" className="w-1/3 my-3 md:my-2"/>
-        <img src={banner2} alt="banner2" className="w-1/3 my-3 md:my-2"/>
-        <img src={banner3} alt="banner3" className="w-1/3 my-3 md:my-2"/>
+        <img src={banner1} alt="banner1" className="w-1/3 my-3 md:my-2" />
+        <img src={banner2} alt="banner2" className="w-1/3 my-3 md:my-2" />
+        <img src={banner3} alt="banner3" className="w-1/3 my-3 md:my-2" />
       </div>
 
       <div>
-        <div className="m-8">
+        <div className="m-4">
           <h1 className="text-2xl text-center md:mb-10 md:text-4xl md:text-center">
             Inspiration for your next adventure
           </h1>
           <div>
-            <div>
-              {places.map((place) => {
-                <div className="bg-gray-200 rounded-lg text-sm p-5 my-5 drop-shadow-[0_15px_25px_rgba(0,0,0,0.02)] hover:drop-shadow-md hover:bg-gray-300">
-                <img src={place} alt="place" />     
-                
-              </div>
-              } )}
+            <div className="my-4 grid gap-x-1 gap-y-2 grid-cols-2 md:grid-cols-4">
+              {places.map((place) => (
+                <div className="border rounded-xl p-3 m-2 text-[10px] md:m-4 md:text-[12px]">
+                  <img src={place} alt="place" />
+                  <div className="flex justify-between mt-2">
+                    <span>Desert King</span>
+                    <b>1MBT per night</b>
+                  </div>
+                  <div className="flex justify-between mt-2">
+                    <span>2345km away</span>
+                    <b>available for 2weeks stay</b>
+                  </div>
+                  {/* <BsStarFill /> */}
+
+                </div>
+              ))}
             </div>
           </div>
         </div>
       </div>
+
+      <div className="bg-fuchsia-800 p-8 md:justify-between md:flex">
+        <div className="w-full mb-10 md:mx-20 md:my-24 md:w-1/2">
+          <h1 className="text-4xl text-white md:mb-8 md:text-5xl">
+            <b>MetaBNB</b> NFTs
+          </h1>
+          <p className="my-2 text-white md:text-xl md:mb-4">
+          Discover our NFT gift cards collection. Loyal customers gets amazing gift cards which are traded as NFTs. These NFTs gives our cutomer access to loads of our exclusive services.
+          </p>
+            <button className="mt-2 px-3 py-2 w-1/3 rounded-lg bg-white text-black hover:bg-fuchsia-600 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+              Learn More
+            </button>
+
+        </div>
+
+        <div className="w-full md:w-1/2 p-4">
+          <img src={heroImage2} alt="home" />
+        </div>
+      </div>
+
+      
     </div>
   );
 };
