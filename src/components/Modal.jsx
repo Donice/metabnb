7a665c74b65b1f6ae39682642eedd811506f2dd1
Donice <doniceubaru1@gmail.com>
2022-11-14 09:07:37 +0100

    fixed bugs in the modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,5 @@
 import { Fragment, useRef, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
-import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
 import Metamask from "./../img/metamask.png";
 import Wallet from "./../img/wallet.png";
 import {MdOutlineArrowForwardIos} from "react-icons/md"
@@ -43,13 +42,7 @@ export default function Modal() {
             >
               <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
                 <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                  <div className="sm:flex sm:items-start">
-                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                      <ExclamationTriangleIcon
-                        className="h-6 w-6 text-red-600"
-                        aria-hidden="true"
-                      />
-                    </div>
+                  <div className="sm:items-start">
                     <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                       <Dialog.Title
                         as="h3"
@@ -57,6 +50,7 @@ export default function Modal() {
                       >
                         Connect Wallet
                       </Dialog.Title>
+                      <hr className="mt-2"/>
                       <div className="mt-2">
                         <p className="text-sm text-gray-500">
                         Choose your preferred wallet:
@@ -65,10 +59,10 @@ export default function Modal() {
                     </div>
                   </div>
                 </div>
-                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                <div className="bg-gray-50 px-4 py-3 sm:px-6">
                   <button
                     type="button"
-                    className="inline-flex w-full justify-between rounded-md border border-gray-300 bg-gray-100 px-4 py-2 text-base font-medium text-black shadow-sm hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+                    className="inline-flex w-full justify-between rounded-md border border-gray-300 bg-gray-100 px-4 py-2 text-base font-medium text-black shadow-sm hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:text-sm"
                     onClick={() => setOpen(false)}
                   >
                     <div className="flex">
@@ -79,7 +73,7 @@ export default function Modal() {
                   </button>
                   <button
                     type="button"
-                    className="mt-3 inline-flex w-full justify-between rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                    className="mt-3 inline-flex w-full justify-between rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-4 sm:text-sm"
                     onClick={() => setOpen(false)}
                     ref={cancelButtonRef}
                   >
